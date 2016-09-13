4d11ee9187fc46a8e2e669566c98c2bbbb3bc676
aomine daiki <evillagomesa@yondu.com>
2016-09-13 10:26:53 +0800

    1

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': '/parkninja-dev/node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
